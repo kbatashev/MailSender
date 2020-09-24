83468d64c7e0478813d5c4ab669d8d95a4aa372a
kbatashev <pony.v.pompone.71@gmail.com>
2020-09-24 22:46:49 +0300

    Набросал классы с конструктором и скелет в дизайнере.

---
+++ Tests/WPFTests/EmailSendServiceClass.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Security;
+
+namespace MailSender
+{
+    public sealed class EmailSendServiceClass
+    {
+        public void Send(string mailUser, SecureString mailPassword)
+        {
+            try
+            {
+                using (var message = new MailMessage(SendMailUserInfo.EmailFrom, SendMailUserInfo.EmailTo, SendMailUserInfo.EmailSubject, SendMailUserInfo.EmailBody))
+                using (var client = new SmtpClient(SendMailUserInfo.Server)
+                {
+                    EnableSsl = true,
+                    Credentials = new NetworkCredential(mailUser, mailPassword)
+                })
+                {
+                    client.Send(message);
+                }
+            }
+            catch(SmtpException error)
+            {
+                throw new SmtpException(error.Message);
+            }
+        }
+    }
+}
